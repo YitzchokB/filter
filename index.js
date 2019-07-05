0ec2ac911f3e0f84f2041d8fd6841b0ed68b0720
YitzchokB <blockyitzchok@gmail.com>
2019-07-04 23:02:01 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import * as nsfwjs from 'nsfwjs'
+
+const test = async () => {
+    const img = document.getElementById('img')
+
+    // Load model from my S3.
+    // See the section hosting the model files on your site.
+    const model = await nsfwjs.load()
+
+    // Classify the image
+    const predictions = await model.classify(img)
+    console.log('Predictions: ', predictions)
+}
+
+test()
